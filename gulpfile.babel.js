ac6033ed76aa77acaf6a0d2080cd53e92391f876
Taylor Radtke <taylorradtke@gmail.com>
2019-04-18 12:15:38 -0400

    fixed gulp build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,21 @@
-import gulp from 'gulp';
-import autoprefixer from 'autoprefixer';
-import browsersync from 'browser-sync';
-import cssnano from 'cssnano';
-import del from 'del';
-import eslint from 'gulp-eslint';
-import htmlmin from 'gulp-htmlmin';
-import imagemin from 'gulp-imagemin';
-import newer from 'gulp-newer';
-import plumber from 'gulp-plumber';
-import postcss from 'gulp-postcss';
-import rename from 'gulp-rename';
-import sass from 'gulp-sass';
-import webpack from 'webpack';
-import webpackstream from 'webpack-stream';
-import babel from 'gulp-babel';
-import sassGlob from 'gulp-sass-glob';
-import sourcemaps from 'gulp-sourcemaps';
-import webpackconfig from './webpack.config';
+import gulp from "gulp";
+import autoprefixer from "autoprefixer";
+import browsersync from "browser-sync";
+import cssnano from "cssnano";
+import del from "del";
+import eslint from "gulp-eslint";
+import htmlmin from "gulp-htmlmin";
+import imagemin from "gulp-imagemin";
+import newer from "gulp-newer";
+import plumber from "gulp-plumber";
+import postcss from "gulp-postcss";
+import rename from "gulp-rename";
+import sass from "gulp-sass";
+import webpack from "webpack";
+import webpackstream from "webpack-stream";
+import sassGlob from "gulp-sass-glob";
+import sourcemaps from "gulp-sourcemaps";
+import webpackconfig from "./webpack.config";
 
 browsersync.create();
 
@@ -32,113 +31,118 @@ project.favicon = `${project.assets}/favicon`;
 
 // BrowserSync
 function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: `${project.dist}/`,
-    },
-    port: 3000,
-  });
-  done();
-}
-
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
+	// watch files
+	const files = [
+		`${project.assets}/*.html`,
+		`${project.scss}/**/*`,
+		`${project.js}/**/*`,
+		`${project.img}/**/*`
+	];
+	browsersync.init(files, {
+		server: {
+			baseDir: `${project.dist}/`
+		},
+		port: 3000,
+		// Inject CSS changes
+		injectChanges: true
+	});
+	done();
 }
 
 // Clean assets
 function clean() {
-  return del([`${project.dist}`]);
+	return del([`${project.dist}`]);
 }
 
 // Copy Favicon Assets
 function favicon() {
-  return gulp.src(`${project.favicon}/**/*`).pipe(gulp.dest(`${project.dist}`));
+	return gulp.src(`${project.favicon}/**/*`).pipe(gulp.dest(`${project.dist}`));
 }
 
 // Minify HTML
 function html() {
-  return gulp
-    .src(`${project.assets}/index.html`)
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest(`${project.dist}`));
+	return gulp
+		.src(`${project.assets}/index.html`)
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest(`${project.dist}`));
 }
 
 // Optimize Images
 function images() {
-  return gulp
-    .src(`${project.img}/**/*`)
-    .pipe(newer(`${project.dist}/img/`))
-    .pipe(
-      imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.jpegtran({ progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [
-            {
-              removeViewBox: false,
-              collapseGroups: true,
-            },
-          ],
-        }),
-      ])
-    )
-    .pipe(gulp.dest(`${project.dist}/img/`));
+	return gulp
+		.src(`${project.img}/**/*`)
+		.pipe(newer(`${project.dist}/img/`))
+		.pipe(
+			imagemin([
+				imagemin.gifsicle({ interlaced: true }),
+				imagemin.jpegtran({ progressive: true }),
+				imagemin.optipng({ optimizationLevel: 5 }),
+				imagemin.svgo({
+					plugins: [
+						{
+							removeViewBox: false,
+							collapseGroups: true
+						}
+					]
+				})
+			])
+		)
+		.pipe(gulp.dest(`${project.dist}/img/`));
 }
 
 // CSS task
 function css() {
-  return gulp
-    .src(`${project.scss}/**/*.scss`)
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sassGlob())
-    .pipe(sass({ outputStyle: 'expanded' }))
-    .pipe(gulp.dest(`${project.dist}/css/`))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest(`${project.dist}/css/`))
-    .pipe(browsersync.stream());
+	return gulp
+		.src(`${project.scss}/**/*.scss`)
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(sassGlob())
+		.pipe(sass({ outputStyle: "expanded" }))
+		.pipe(gulp.dest(`${project.dist}/css/`))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(gulp.dest(`${project.dist}/css/`))
+		.pipe(browsersync.stream());
 }
 
 // Lint scripts
 function scriptsLint() {
-  return gulp
-    .src([`${project.js}/**/*`, './gulpfile.babel.js'])
-    .pipe(plumber())
-    .pipe(eslint())
-    .pipe(eslint.format());
-  // .pipe(eslint.failAfterError());
+	return gulp
+		.src([`${project.js}/**/*`, "./gulpfile.babel.js"])
+		.pipe(plumber())
+		.pipe(eslint())
+		.pipe(eslint.format());
+	// .pipe(eslint.failAfterError());
 }
 
 // Transpile, concatenate and minify scripts
 function scripts() {
-  return (
-    gulp
-      .src([`${project.js}/**/*`])
-      .pipe(babel())
-      .pipe(plumber())
-      .pipe(webpackstream(webpackconfig, webpack))
-      // folder only, filename is specified in webpack config
-      .pipe(gulp.dest(`${project.dist}/js/`))
-      .pipe(browsersync.stream())
-  );
+	return (
+		gulp
+			.src([`${project.js}/**/*`])
+			.pipe(plumber())
+			.pipe(webpackstream(webpackconfig, webpack))
+			// folder only, filename is specified in webpack config
+			.pipe(gulp.dest(`${project.dist}/js/`))
+			.pipe(browsersync.stream())
+	);
 }
 
 // Watch files
-function watchFiles() {
-  gulp.watch(`${project.assets}/*.html`, html);
-  gulp.watch(`${project.scss}/**/*`, css);
-  gulp.watch(`${project.js}/**/*`, gulp.series(scriptsLint, scripts));
-  gulp.watch(`${project.img}/**/*`, images);
+function serveFiles() {
+	gulp.watch(`${project.assets}/*.html`, html);
+	gulp.watch(`${project.scss}/**/*`, css);
+	gulp.watch(`${project.js}/**/*`, gulp.series(scriptsLint, scripts));
+	gulp.watch(`${project.img}/**/*`, images);
 }
 
 // define complex tasks
 const js = gulp.series(scriptsLint, scripts);
-const build = gulp.series(clean, gulp.parallel(html, css, favicon, images, js));
-const watch = gulp.parallel(watchFiles, browserSync);
+const build = gulp.series(
+	clean,
+	gulp.parallel(html, css, favicon, images, js)
+);
+const watch = gulp.parallel(serveFiles, browserSync);
 
 // export tasks
 export { html, images, favicon, css, js, clean, build, watch };
